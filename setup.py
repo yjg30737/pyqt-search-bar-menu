b77784cddb4cc64069b2e527c2a2d8d8e1e91871
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-13 17:53:02 +0900

    Update the setup.py to upload this to pypi

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-search-bar-menu',
-    version='0.1.0',
+    version='0.0.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
     description='PyQt QMenu which contains search bar as first item',
     url='https://github.com/yjg30737/pyqt-search-bar-menu.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8',
-        'pyqt-auto-search-bar @ git+https://git@github.com/yjg30737/pyqt-auto-search-bar.git@main'
+        'pyqt-auto-search-bar>=0.0.1'
     ]
 )
